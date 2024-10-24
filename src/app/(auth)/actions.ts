bed79b505b04b0479a45489250e8569a020ee92f
Bsjaehee <bsjaehee94@gmail.com>
2024-10-24 14:02:17 +0900

    feature: 로그인,회원가입 폼 react-hook-form 추가

--- src/app/(auth)/actions.ts
+++ src/app/(auth)/actions.ts
@@ -3,22 +3,24 @@ import { redirect } from "next/navigation";
 import { createClient } from "../utils/supabase/server";
 import { SignUpWithPasswordCredentials } from "@supabase/supabase-js";
 import { cookies } from "next/headers";
+import { SignupInput } from "./signup/components/SignupForm";
+import { LoginInput } from "./login/components/LoginForm";
 
 // 로그인
-export async function login(formData: FormData) {
+export async function login(loginInput: LoginInput) {
   const supabase = createClient();
   const data = {
-    email: formData.get("email") as string,
-    password: formData.get("password") as string
+    email: loginInput.email,
+    password: loginInput.password
   };
   const { error } = await supabase.auth.signInWithPassword(data);
   if (error) {
-    console.log("로그인 오류", error);
+    console.error("로그인 오류", error);
+    throw new Error(error.message);
   }
-
   const { data: userData, error: userError } = await supabase.auth.getUser();
   if (userError) {
-    console.log("유저 정보 가져오기 오류:", userError);
+    console.error("유저 정보 가져오기 오류:", userError);
   } else {
     console.log("로그인된 유저:", userData.user); // 유저 정보 출력
   }
@@ -26,20 +28,21 @@ export async function login(formData: FormData) {
 }
 
 // 회원가입
-export async function signup(formData: FormData) {
+export async function signup(signupInput: SignupInput) {
   const supabase = createClient();
   const data: SignUpWithPasswordCredentials = {
-    email: formData.get("email") as string,
-    password: formData.get("password") as string,
+    email: signupInput.email,
+    password: signupInput.password,
     options: {
       data: {
-        nickname: formData.get("nickname") as string
+        nickname: signupInput.nickname
       }
     }
   };
   const { error } = await supabase.auth.signUp(data);
   if (error) {
     console.error("회원가입 에러", error);
+  } else {
+    redirect("/login");
   }
-  redirect("/login");
 }
--- src/app/(auth)/signup/components/SignupForm.tsx
+++ src/app/(auth)/signup/components/SignupForm.tsx
@@ -1,16 +1,70 @@
 "use client";
-
 import { signup } from "../../actions";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+// Zod 스키마 정의
+const signupSchema = z.object({
+  email: z.string().email({ message: "유효한 이메일을 입력해주세요." }),
+  password: z
+    .string()
+    .min(6, { message: "비밀번호는 최소 6자 이상이어야 합니다." })
+    .nonempty({ message: "비밀번호를 입력해주세요." }),
+  nickname: z
+    .string()
+    .min(2, { message: "닉네임은 최소 2자 이상이어야 합니다." })
+    .max(20, { message: "닉네임은 20자 이하이어야 합니다." }) // 닉네임 길이 제한
+    .regex(/^[a-zA-Z0-9가-힣@_-]*$/, {
+      message:
+        "닉네임은 알파벳, 숫자, 한글, @, 밑줄 및 하이픈만 포함해야 합니다."
+    }) // 특정 문자만 허용
+});
+
+export interface SignupInput {
+  email: string;
+  password: string;
+  nickname: string;
+}
 
 const SignupForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<SignupInput>({
+    resolver: zodResolver(signupSchema)
+  });
+
+  const onSubmit: SubmitHandler<SignupInput> = async (data: SignupInput) => {
+    console.log("제출된 데이터:", data); // 데이터 로그
+    try {
+      await signup(data);
+      alert("회원가입이 완료되었습니다.");
+      console.log("회원가입 성공");
+    } catch (error) {
+      console.error("회원가입 오류:", error);
+    }
+  };
+
   return (
-    <form className="flex flex-col justify-center items-center">
-      <input type="email" name="email" placeholder="email" />
-      <input type="password" name="password" placeholder="password" />
-      <input type="text" name="nickname" placeholder="nickname" />
-      <button type="submit" formAction={signup}>
-        회원가입
-      </button>
+    <form
+      className="flex flex-col justify-center items-center"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <input type="email" {...register("email")} placeholder="email" />
+      <p role="alert" className="text-red-600">
+        {errors.email?.message}
+      </p>
+      <input type="password" {...register("password")} placeholder="password" />
+      <p role="alert" className="text-red-600">
+        {errors.password?.message}
+      </p>
+      <input type="text" {...register("nickname")} placeholder="nickname" />
+      <p role="alert" className="text-red-600">
+        {errors.nickname?.message}
+      </p>
+      <button type="submit">회원가입</button>
     </form>
   );
 };
