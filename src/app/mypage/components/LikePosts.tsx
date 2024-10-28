d5fb3f3af32f1794ea27d06aa40046f9edf5f79a
Bsjaehee <bsjaehee94@gmail.com>
2024-10-28 20:25:47 +0900

    feature: 라이크포스트, 프로필이미지업로드 추가

---
+++ src/app/mypage/components/LikePosts.tsx
@@ -0,0 +1,26 @@
+import { getLikePosts } from "@/api/user-action";
+import { User } from "@supabase/supabase-js";
+
+interface LikePostsProps {
+  user: User;
+}
+
+const LikePosts = async ({ user }: LikePostsProps) => {
+  const likeInfo = await getLikePosts(user.id);
+  const posts = likeInfo?.map((like) => like.posts).flat(); // 모든 like에서 posts 추출 후 평탄화
+
+  console.log("like 테이블 연결--------->", likeInfo);
+  console.log("ddddddd", posts);
+
+  return (
+    <div className="bg-slate-400">
+      {posts?.map((post) => (
+        <div key={post.post_id}>
+          <p>title: {post.post_title}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default LikePosts;
