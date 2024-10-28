c2753086179c32a86bcd9e8d5436c8eab9dc8855
Bsjaehee <bsjaehee94@gmail.com>
2024-10-28 18:45:36 +0900

    add: mypost 페이지 추가

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,23 +1,47 @@
-import React from "react";
 import DeleteAccountButton from "../(auth)/login/components/DeleteAccountButton";
 import LogoutButton from "../(auth)/login/components/LogoutButton";
 import { getUser } from "@/api/auth-actions";
 import UserInfoCard from "./components/UserInfoCard";
-import Myposts from "./components/Myposts";
+import MyPoint from "./components/MyPoint";
+import Link from "next/link";
+import LikePosts from "./components/LikePosts";
 
 const Mypage = async () => {
   const user = await getUser();
   if (!user) return;
-  console.log("current user---------->", user);
 
   return (
-    <div className="w-1/3 flex flex-col m-auto">
+    <div className="w-1/3 flex flex-col m-auto w-full px-10">
       <h1>마이페이지</h1>
       <p>포인트와 등급을 확인해보세요!</p>
-      <UserInfoCard user={user} />
-      <DeleteAccountButton user={user} />
-      <LogoutButton />
-      <Myposts user={user} />
+      <div className="flex flex-row justify-center gap-5">
+        <UserInfoCard user={user} />
+        <MyPoint user={user} />
+      </div>
+
+      <div className="border-b border-gray-400 mt-10 mb-1 flex justify-between items-center p-4">
+        <Link href={"/"}>챌린지 히스토리</Link>
+      </div>
+      <div className="border-b border-gray-400 mb-1 flex justify-between items-center p-4">
+        <Link href={"/"}>탄소계산 히스토리</Link>
+      </div>
+      <div className="border-b border-gray-400 mb-1 flex justify-between items-center p-4">
+        <Link href={"/"}>나의 스크랩</Link>
+        <LikePosts user={user} />
+      </div>
+      <div className="border-b border-gray-400 mb-1 flex justify-between items-center p-4">
+        <Link href={"/mypage/post"}>나의 게시글</Link>
+      </div>
+      <div className="border-b border-gray-400 mb-1 flex justify-between items-center p-4">
+        <Link href={"/"}>회원정보 수정</Link>
+      </div>
+      <div className="border-b border-gray-400 mb-1 flex justify-between items-center p-4">
+        <Link href={"/"}>회원 탈퇴</Link>
+      </div>
+      <div className="mt-10">
+        <DeleteAccountButton user={user} />
+        <LogoutButton />
+      </div>
     </div>
   );
 };
