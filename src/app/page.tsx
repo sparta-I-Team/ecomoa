6f8e45148e8c70a5d2bdb89e372b94caba2e8939
Bsjaehee <bsjaehee94@gmail.com>
2024-10-31 14:54:05 +0900

    chore: 메인페이지 복구

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import { getSession } from "@/api/auth-actions";
-import { getUserInfo } from "@/api/user-action";
-import NicknameModal from "./(auth)/login/components/NicknameModal";
-
-export default async function Home() {
-  const session = await getSession();
-  if (!session) return;
-  const userInfo = await getUserInfo(session?.user.id);
-  if (!userInfo.params || Object.keys(userInfo.params).length === 0) {
-    return <NicknameModal />;
-  }
+export default function Home() {
   return <>Home</>;
 }
