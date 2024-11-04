e844b29f06f874ca59d8d5b48431f95ef44e1d39
RYC0208 <ryc0208@naver.com>
2024-11-05 03:01:15 +0900

    refactor: post/page.tsx 수정

--- src/app/community/post/page.tsx
+++ src/app/community/post/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 import React, { useState, useEffect, useRef } from "react";
-import { User } from "@supabase/supabase-js";
 import Link from "next/link";
 import Image from "next/image";
 import { createClient } from "@/utlis/supabase/client";
+import { useModalStore } from "@/zustand/modalStore";
+import { userStore } from "@/zustand/userStore";
+import { getUserInfo } from "@/api/user-action";
+import { UserInfo } from "@/types/userInfoType";
+import { Modal } from "@/components/shared/Modal";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
@@ -15,7 +19,7 @@ if (!supabaseUrl || !supabaseAnonKey) {
 const supabase = createClient();
 
 const PostPage = () => {
-  const [user, setUser] = useState<User | null>(null);
+  const [userinfo, setUserinfo] = useState<UserInfo | null>(null);
   const [userNickname, setUserNickname] = useState("");
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
@@ -25,36 +29,48 @@ const PostPage = () => {
   );
   const [errorMessage, setErrorMessage] = useState("");
   const [uploadedImageUrls, setUploadedImageUrls] = useState<string[]>([]);
-  const [isModalVisible, setIsModalVisible] = useState(false);
+  // const [isModalVisible, setIsModalVisible] = useState(false);
   const fileInputRef = useRef<HTMLInputElement | null>(null);
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      const {
-        data: { user },
-        error
-      } = await supabase.auth.getUser();
-      if (error) {
-        console.error("사용자 정보 가져오기 오류:", error.message);
-      } else {
-        setUser(user);
-        if (user) {
-          const { data, error } = await supabase
-            .from("user_info")
-            .select("user_nickname")
-            .eq("user_id", user.id)
-            .single();
-          if (data) {
-            setUserNickname(data.user_nickname);
-          } else {
-            console.error("닉네임을 가져오는 데 실패했습니다:", error.message);
-          }
-        }
-      }
-    };
+  const { openModal, closeModal } = useModalStore();
 
-    fetchUser();
-  }, []);
+  const { user } = userStore();
+
+  useEffect(() => {
+    const fetch = async () => {
+      const res = await getUserInfo(user.id);
+      setUserinfo(res);
+    };
+    fetch();
+  }, [user]);
+
+  // useEffect(() => {
+  //   const fetchUser = async () => {
+  //     const {
+  //       data: { user },
+  //       error
+  //     } = await supabase.auth.getUser();
+  //     if (error) {
+  //       console.error("사용자 정보 가져오기 오류:", error.message);
+  //     } else {
+  //       setUser(user);
+  //       if (user) {
+  //         const { data, error } = await supabase
+  //           .from("user_info")
+  //           .select("user_nickname")
+  //           .eq("user_id", user.id)
+  //           .single();
+  //         if (data) {
+  //           setUserNickname(data.user_nickname);
+  //         } else {
+  //           console.error("닉네임을 가져오는 데 실패했습니다:", error.message);
+  //         }
+  //       }
+  //     }
+  //   };
+
+  //   fetchUser();
+  // }, []);
 
   const handleImageChange = (
     event: React.ChangeEvent<HTMLInputElement>,
@@ -88,7 +104,7 @@ const PostPage = () => {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if (!user) {
+    if (!userinfo) {
       setErrorMessage("사용자가 인증되지 않았습니다.");
       return;
     }
@@ -126,7 +142,7 @@ const PostPage = () => {
 
     const { error } = await supabase.from("posts").insert([
       {
-        user_id: user.id,
+        user_id: userinfo.user_id,
         post_title: title,
         post_content: content,
         created_at: new Date().toISOString(),
@@ -134,20 +150,59 @@ const PostPage = () => {
       }
     ]);
 
+    openModal(
+      <div>
+        <div className="bg-white p-6 rounded-lg shadow-lg text-center relative w-[585px] h-[600px]">
+          <button
+            onClick={closeModal}
+            className="absolute top-2 right-2 text-gray-600 hover:text-gray-900 border-none text-3xl"
+          >
+            &times; {/* X 아이콘 */}
+          </button>
+          <div className="p-12">
+            <h4 className="font-semibold mb-4 text-2xl">
+              게시글을 업로드 했어요
+            </h4>
+            {uploadedImageUrls[0] ? (
+              <Image
+                src={uploadedImageUrls[0]}
+                alt="등록한 이미지"
+                width={300}
+                height={260}
+                className="mb-4 max-w-full rounded"
+              />
+            ) : (
+              <div className="text-gray-500 ">등록된 이미지가 없습니다.</div>
+            )}
+            <h4 className=" py-2 px-3">위치</h4>
+            <h3>{"마이페이지 > 나의 게시글 > 자유게시판 "} </h3>
+            <Link
+              href="/community"
+              className="mt-4 p-2 bg-black text-white rounded "
+            >
+              업로드한 게시글 보러가기
+            </Link>
+          </div>
+        </div>
+      </div>,
+      "",
+      0
+    );
+
     if (error) {
       console.error("게시글 생성 오류:", error.message);
       setErrorMessage("게시글 등록에 실패했습니다.");
     } else {
       resetForm();
-      setIsModalVisible(true);
+      // setIsModalVisible(true);
       setUploadedImageUrls(uploadedUrls);
     }
   };
 
-  const closeModal = () => {
-    setIsModalVisible(false);
-    setUploadedImageUrls([]);
-  };
+  // const closeModal = () => {
+  //   setIsModalVisible(false);
+  //   setUploadedImageUrls([]);
+  // };
 
   return (
     <main>
@@ -222,55 +277,56 @@ const PostPage = () => {
         </button>
       </form>
 
-      {isModalVisible && (
+      {/* {isModalVisible && (
         <Modal imageUrl={uploadedImageUrls[0]} onClose={closeModal} />
-      )}
+      )} */}
+      <Modal />
     </main>
   );
 };
 
-interface ModalProps {
-  imageUrl: string | null;
-  onClose: () => void;
-}
+// interface ModalProps {
+//   imageUrl: string | null;
+//   // onClose: () => void;
+// }
 
-const Modal: React.FC<ModalProps> = ({ imageUrl, onClose }) => {
-  return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg text-center relative w-[585px] h-[600px]">
-        <button
-          onClick={onClose}
-          className="absolute top-2 right-2 text-gray-600 hover:text-gray-900 border-none text-3xl"
-        >
-          &times; {/* X 아이콘 */}
-        </button>
-        <div className="p-12">
-          <h4 className="font-semibold mb-4 text-2xl">
-            게시글을 업로드 했어요
-          </h4>
-          {imageUrl ? (
-            <Image
-              src={imageUrl}
-              alt="등록한 이미지"
-              width={300}
-              height={260}
-              className="mb-4 max-w-full rounded"
-            />
-          ) : (
-            <div className="text-gray-500 ">등록된 이미지가 없습니다.</div>
-          )}
-          <h4 className=" py-2 px-3">위치</h4>
-          <h3>{"마이페이지 > 나의 게시글 > 자유게시판 "} </h3>
-          <Link
-            href="/community"
-            className="mt-4 p-2 bg-black text-white rounded "
-          >
-            업로드한 게시글 보러가기
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
-};
+// const Modal: React.FC<ModalProps> = ({ imageUrl, onClose }) => {
+//   return (
+//     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+//       <div className="bg-white p-6 rounded-lg shadow-lg text-center relative w-[585px] h-[600px]">
+//         <button
+//           onClick={onClose}
+//           className="absolute top-2 right-2 text-gray-600 hover:text-gray-900 border-none text-3xl"
+//         >
+//           &times; {/* X 아이콘 */}
+//         </button>
+//         <div className="p-12">
+//           <h4 className="font-semibold mb-4 text-2xl">
+//             게시글을 업로드 했어요
+//           </h4>
+//           {imageUrl ? (
+//             <Image
+//               src={imageUrl}
+//               alt="등록한 이미지"
+//               width={300}
+//               height={260}
+//               className="mb-4 max-w-full rounded"
+//             />
+//           ) : (
+//             <div className="text-gray-500 ">등록된 이미지가 없습니다.</div>
+//           )}
+//           <h4 className=" py-2 px-3">위치</h4>
+//           <h3>{"마이페이지 > 나의 게시글 > 자유게시판 "} </h3>
+//           <Link
+//             href="/community"
+//             className="mt-4 p-2 bg-black text-white rounded "
+//           >
+//             업로드한 게시글 보러가기
+//           </Link>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 export default PostPage;
