0d6cb0603a637c28d1e8ac99be93b259716fab45
Bsjaehee <bsjaehee94@gmail.com>
2024-11-04 15:24:11 +0900

    chore: 미들웽어 수정

--- src/utlis/supabase/middleware.ts
+++ src/utlis/supabase/middleware.ts
@@ -36,18 +36,11 @@ export async function updateSession(request: NextRequest) {
   const {
     data: { user }
   } = await supabase.auth.getUser();
-  console.log("유저@@", user);
-  console.log("Request path:", request.nextUrl.pathname);
   if (
     !user &&
-    request.nextUrl.pathname.startsWith("/mypage")
-    // request.nextUrl.pathname.startsWith("/challenge")
-    // !request.nextUrl.pathname.startsWith("/login") &&
-    // !request.nextUrl.pathname.startsWith("/") &&
-    // !request.nextUrl.pathname.startsWith("/signup") &&
+    request.nextUrl.pathname.startsWith("/mypage") &&
+    request.nextUrl.pathname.startsWith("/challenge")
   ) {
-    // console.log("Redirecting to /login"); // 리다이렉션 발생 시 로그 남기기
-    // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
     url.pathname = "/login";
     return NextResponse.redirect(url);
